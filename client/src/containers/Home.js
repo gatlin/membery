d09beb00356fe8df4291677021e0612cc2ec0c54
Gatlin Johnson <gatlin@niltag.net>
2019-03-14 12:22:46 -0500

    Helps to commit all the files

---
+++ client/src/containers/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../Auth';
+import { Home } from '../components/Home';
+
+export default connect(
+    (state) => (state),
+    dispatch => ({
+        login: () => dispatch(actions.login()),
+        isAuthenticated: () => dispatch(actions.isAuthenticated())
+    })
+)(Home);
